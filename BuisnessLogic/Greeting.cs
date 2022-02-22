e6ae80853f6902ca964db4a7cdc5e436058f068f
EPAM\Maria_Zimnoch <maria_zimnoch@epam.com>
2022-02-22 09:21:59 +0100

    Add project files.

---
+++ BuisnessLogic/Greeting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BusinessLogic
+{
+    public static class Greeting
+    {
+        public static void Welcome()
+        {
+            Console.WriteLine("Your name is...");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"{date.ToShortTimeString()} {date.ToShortDateString()} Hello, {name}!");
+        }
+    }
+}
